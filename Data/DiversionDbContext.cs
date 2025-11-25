976c0ff6773d8fcd15ee00e7f31da2b93c7f0f88
Chris <christopherpack17@gmail.com>
2025-11-25 09:32:01 -0600

    Add project files.

---
+++ Data/DiversionDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Diversion.Data
+{
+    public class DiversionDbContext(DbContextOptions<DiversionDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
