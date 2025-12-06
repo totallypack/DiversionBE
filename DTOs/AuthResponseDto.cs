cde4fe79d880fbc7dda3e65804ce6d122a4f8cdf
Chris <christopherpack17@gmail.com>
2025-12-06 16:24:35 -0600

    Auth Flow

--- DTOs/AuthResponseDto.cs
+++ DTOs/AuthResponseDto.cs
@@ -2,9 +2,7 @@
 {
     public class AuthResponseDto
     {
-        public string? Token { get; set; }
         public string? Username { get; set; }
         public string? Email { get; set; }
-        public DateTime Expiration {  get; set; }
     }
 }
---
+++ Migrations/20251206201132_UpdateUserProfileSchema.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Diversion.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserProfileSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "UserProfiles",
+                newName: "DisplayName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisplayName",
+                table: "UserProfiles",
+                newName: "UserName");
+        }
+    }
+}
