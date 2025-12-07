bcf80fba1275f06c51d3f7d1cbf3d64239b3980c
Chris <christopherpack17@gmail.com>
2025-12-06 20:15:06 -0600

    Better nav and details

--- Controllers/SubInterestsController.cs
+++ Controllers/SubInterestsController.cs
@@ -52,23 +52,5 @@ namespace Diversion.Controllers
                 return NotFound();
             return Ok(subInterest);
         }
-
-        [HttpGet("interest/{interestId}")]
-        public async Task<ActionResult<IEnumerable<SubInterestDto>>> GetSubInterestsByInterest(Guid interestId)
-        {
-            var subInterests = await _context.SubInterests
-                .Where(si => si.InterestId == interestId)
-                .Select(si => new SubInterestDto
-                {
-                    Id = si.Id,
-                    Name = si.Name,
-                    InterestId = si.InterestId,
-                    Description = si.Description,
-                    IconUrl = si.IconUrl
-                })
-                .ToListAsync();
-
-            return Ok(subInterests);
-        }
     }
 }
